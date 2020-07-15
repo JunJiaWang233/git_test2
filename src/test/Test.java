96462e0ab57f116b61501c8dca16412b29de4111
junjia <285509@via.dk>
2020-07-15 14:59:10 +0200

    aaaaaaaa

--- src/test/Test.java
+++ src/test/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("this is a git project");
         System.out.println("1");
+        System.out.println("aaaaaaaaaa");
     }
 }
